57d8c1cccb7a028430323ccb2f5ff6776ccbe87a
ulises <gerguaigua@gmail.com>
2020-10-27 19:36:44 -0300

    Quitando .cache

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/ulises/app/guaigua.digital/.cache/dev-404-page.js"))),
+  "component---src-pages-2-js": hot(preferDefault(require("/home/ulises/app/guaigua.digital/src/pages/2.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/ulises/app/guaigua.digital/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/ulises/app/guaigua.digital/src/pages/index.js")))
 }
